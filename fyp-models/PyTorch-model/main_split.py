4e6179dacccbdd3112a5cc329d51e1fae1ddb3d4
Benny Bickerton <bbic0004@student.monash.edu>
2025-04-16 14:19:16 +1000

    Uploaded Guohui's PyTorch model.

---
+++ fyp-models/PyTorch-model/main_split.py
@@ -0,0 +1,10 @@
+from train_split import train_model
+from test_split import test_model
+
+train_dir = "dataset/train"
+val_dir = "dataset/val"
+test_dir = "dataset/test"
+
+if __name__ == "__main__":
+    train_model(train_dir, val_dir)
+    test_model(test_dir)
